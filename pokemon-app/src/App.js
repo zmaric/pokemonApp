665e900af907b26c99db949bc15799c4b52bd2d4
zmaric <z_maric@icloud.com>
2022-01-06 15:00:13 +0100

    installed axios, fetched data from pokemon api and displyed it on page

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,8 +1,31 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [pokemons, setPokemons] = useState([])
+
+  // used useEffect to fetch data only first time after page loads
+  // inside is created function that fetch data with axios and set data to pokemons array
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=200')
+      setPokemons(res.data.results)
+    }
+    getData()
+  }, [])
+
+
   return (
-    <div className="App">
-      <p>pokemon app</p>
+    <div className="All pokemons">
+      <form style={{margin: "auto", width: "60%", textAlign: "center"}}>
+        <h3>Pokemons</h3>
+        {(pokemons.length > 0) ? pokemons.map((item, key) => (
+          <div key={key}>
+            <p>{item.name}</p>
+          </div>
+        )) : <p>Loading...</p> }
+      </form>
     </div>
   );
 }
